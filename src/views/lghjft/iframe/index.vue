1644c0f4daa65a82812750a7581748fb48f9d4e3
zhf <zhf@63aaa323-bd20-4782-bdcf-f624a29d9816>
2026-01-10 13:12:27 +0000

    feat: update lghjft iframe component
    git-svn-id: svn://172.16.27.209/gsgh/gsgh/lghjft-frontend@532 63aaa323-bd20-4782-bdcf-f624a29d9816

--- src/views/lghjft/iframe/index.vue
+++ src/views/lghjft/iframe/index.vue
@@ -6,12 +6,24 @@ import { IFrame } from '@/components/IFrame'
 import { useRoute } from 'vue-router'
 import { computed } from 'vue'
 import { useTagsViewStore } from '@/store/modules/tagsView'
+import { getAccessToken } from '@/utils/auth'
 
 defineOptions({ name: 'LghjftIFrame' })
 
 const route = useRoute()
 const tagsViewStore = useTagsViewStore()
-const url = computed(() => route.query.url as string)
+const url = computed(() => {
+  let u = route.query.url as string
+  const token = getAccessToken()
+  if (token) {
+    if (u.indexOf('?') === -1) {
+      u += '?token=' + token
+    } else {
+      u += '&token=' + token
+    }
+  }
+  return u
+})
 const title = computed(() => route.query.title as string)
 
 if (title.value) {
